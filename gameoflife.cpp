fc1309d78b9334af079d859f9f50412ec59de176
Philip Rebeschini <philiprebeschini7@gmail.com>
2017-10-18 20:09:40 +0200

    Changing some variable names.

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -3,15 +3,13 @@
 // TODO Template generation size
 // TODO Disable copy initialization/assignment or do deep copying/assignment
 // TODO Instead of bool values for each cell use enum class with Alive/Dead
-// TODO Change i, j iteration variables to row, column
-// TODO Change variables names current_gen, future_gen to generation_...
 class Species {
 
 private:
     bool generation_a [10][10] {};
     bool generation_b [10][10] {};
-    bool (*current_generation)[10] {generation_a};
-    bool (*future_generation)[10]  {generation_b};
+    bool (*generation_current)[10] {generation_a};
+    bool (*generation_future)[10]  {generation_b};
 
     bool alive(int, int) const;
     int count_alive_neighbours(int, int) const;
@@ -33,7 +31,7 @@ Species::Species(bool (*initial_generation)[10]) {
 
 bool Species::alive(int row, int column) const {
     // TODO throw exception if row, column out of bounds
-    return current_generation[row][column];
+    return generation_current[row][column];
 }
 
 int Species::count_alive_neighbours(int row, int column) const {
@@ -118,7 +116,7 @@ int Species::count_alive_neighbours(int row, int column) const {
 // TODO Remove generation parameter, we only want to change future states.
 void Species::change_state(int row, int column, bool state) {
     // TODO throw exception if row, column out of bounds
-    future_generation[row][column] = state;
+    generation_future[row][column] = state;
 }
 
 // Checks for every cell if it can live on to the next generation
@@ -137,9 +135,9 @@ void Species::evolve() {
         }
     }
     // Recycle generations
-    bool (*temp_generation)[10] = current_generation;
-    current_generation = future_generation;
-    future_generation  = temp_generation;
+    bool (*generation_temporary)[10] = generation_current;
+    generation_current = generation_future;
+    generation_future  = generation_temporary;
 }
 
 std::ostream& operator<<(std::ostream &out, const Species &species) {
