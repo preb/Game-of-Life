f20f6f4a0da4701ad31cfa48b32125326dd3e260
Philip Rebeschini <philiprebeschini7@gmail.com>
2017-10-19 10:50:10 +0200

    Adding template to vary grid size

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,8 +1,8 @@
 #include <array>
 #include <iostream>
 
-// TODO Template generation size
-// TODO Instead of bool values for each cell use enum class with Alive/Dead
+// TODO use seperate files for class
+template <int size>
 class Species {
 
 public:
@@ -14,7 +14,7 @@ public:
 
 private:
 
-    using grid = std::array<std::array<Cell, 10>, 10>;
+    using grid = std::array<std::array<Cell, size>, size>;
 
     grid generation_a {};
     grid generation_b {};
@@ -29,36 +29,42 @@ private:
 public:
 
     Species(const grid&);
-    Species(const Species&);
-    Species& operator=(const Species&);
+    Species(const Species<size>&);
+    Species<size>& operator=(const Species<size>&);
 
     void evolve();
-    friend std::ostream& operator<<(std::ostream&, const Species&);
+
+    template <int samesize>
+    friend std::ostream& operator<<(std::ostream&, const Species<samesize>&);
 };
 
-void Species::change_state(int row, int column, Cell state) {
+template <int size>
+void Species<size>::change_state(int row, int column, Cell state) {
     // TODO throw exception if row, column out of bounds
     (*generation_future)[row][column] = state;
 }
 
-bool Species::alive(int row, int column) const {
+template <int size>
+bool Species<size>::alive(int row, int column) const {
     // TODO throw exception if row, column out of bounds
     return (*generation_current)[row][column] == Cell::ALIVE;
 }
 
-int Species::count_alive_neighbours(int row, int column) const {
+template <int size>
+int Species<size>::count_alive_neighbours(int row, int column) const {
     // TODO throw exception if row, column out of bounds
+    const int edge {size - 1};
     int alive_neighbours {0};
 
     // Check north
     if (row == 0) {
-        if (alive(9, column)) ++alive_neighbours;
+        if (alive(edge, column)) ++alive_neighbours;
     } else {
         if (alive(row - 1, column)) ++alive_neighbours;
     }
 
     // Check south
-    if (row == 9) {
+    if (row == edge) {
         if (alive(0, column)) ++alive_neighbours;
     } else {
         if (alive(row + 1, column)) ++alive_neighbours;
@@ -66,13 +72,13 @@ int Species::count_alive_neighbours(int row, int column) const {
 
     // Check west
     if (column == 0) {
-        if (alive(row, 9)) ++alive_neighbours;
+        if (alive(row, edge)) ++alive_neighbours;
     } else {
         if (alive(row, column - 1)) ++alive_neighbours;
     }
 
     // Check east
-    if (column == 9) {
+    if (column == edge) {
         if (alive(row, 0)) ++alive_neighbours;
     } else {
         if (alive(row, column + 1)) ++alive_neighbours;
@@ -80,43 +86,43 @@ int Species::count_alive_neighbours(int row, int column) const {
 
     // Check north west
     if (row == 0 && column == 0) {
-        if (alive(9, 9)) ++alive_neighbours;
+        if (alive(edge, edge)) ++alive_neighbours;
     } else if (row == 0) {
-        if (alive(9, column - 1)) ++alive_neighbours;
+        if (alive(edge, column - 1)) ++alive_neighbours;
     } else if (column == 0) {
-        if (alive(row - 1, 9)) ++alive_neighbours;
+        if (alive(row - 1, edge)) ++alive_neighbours;
     } else {
         if (alive(row - 1, column - 1)) ++alive_neighbours;
     }
 
     // Check north east
-    if (row == 0 && column == 9) {
-        if (alive(9, 0)) ++alive_neighbours;
+    if (row == 0 && column == edge) {
+        if (alive(edge, 0)) ++alive_neighbours;
     } else if (row == 0) {
-        if (alive(9, column + 1)) ++alive_neighbours;
-    } else if (column == 9) {
+        if (alive(edge, column + 1)) ++alive_neighbours;
+    } else if (column == edge) {
         if (alive(row - 1, 0)) ++alive_neighbours;
     } else  {
         if (alive(row - 1, column + 1)) ++alive_neighbours;
     }
 
     // Check south west
-    if (row == 9 && column == 0) {
-        if (alive(0, 9)) ++alive_neighbours;
-    } else if (row == 9) {
+    if (row == edge && column == 0) {
+        if (alive(0, edge)) ++alive_neighbours;
+    } else if (row == edge) {
         if (alive(0, column - 1)) ++alive_neighbours;
     } else if (column == 0) {
-        if (alive(row + 1, 9)) ++alive_neighbours;
+        if (alive(row + 1, edge)) ++alive_neighbours;
     } else {
         if (alive(row + 1, column - 1)) ++alive_neighbours;
     }
 
     // Check south east
-    if (row == 9 && column == 9) {
+    if (row == edge && column == edge) {
         if (alive(0, 0)) ++alive_neighbours;
-    } else if (row == 9) {
+    } else if (row == edge) {
         if (alive(0, column + 1)) ++alive_neighbours;
-    } else if (column == 9) {
+    } else if (column == edge) {
         if (alive(row + 1, 0)) ++alive_neighbours;
     } else {
         if (alive(row + 1, column + 1)) ++alive_neighbours;
@@ -125,21 +131,24 @@ int Species::count_alive_neighbours(int row, int column) const {
     return alive_neighbours;
 }
 
-Species::Species(const grid& generation_initial) {
-    for (int row {0}; row < 10; ++row) {
-        for (int column {0}; column < 10; ++column) {
+template <int size>
+Species<size>::Species(const grid& generation_initial) {
+    for (int row {0}; row < size; ++row) {
+        for (int column {0}; column < size; ++column) {
             generation_a[row][column] = generation_initial[row][column];
         }
     }
 }
 
-Species::Species(const Species& other) {
+template <int size>
+Species<size>::Species(const Species& other) {
     if (this != &other) {
         *generation_current = *other.generation_current;
     }
 }
 
-Species& Species::operator=(const Species& other) {
+template <int size>
+Species<size>& Species<size>::operator=(const Species& other) {
     if (this != &other) {
         *generation_current = *other.generation_current;
     }
@@ -150,9 +159,10 @@ Species& Species::operator=(const Species& other) {
 // with the help of its neighbours or if it dies.
 // The evolved state for every cell is set for the future generation,
 // which then becomes the current generation.
-void Species::evolve() {
-    for (int row {0}; row < 10; ++row) {
-        for (int column {0}; column < 10; ++column) {
+template <int size>
+void Species<size>::evolve() {
+    for (int row {0}; row < size; ++row) {
+        for (int column {0}; column < size; ++column) {
             int alive_neighbours {count_alive_neighbours(row, column)};
             if (alive_neighbours < 2 || alive_neighbours > 3) {
                 change_state(row, column, Cell::DEAD);
@@ -167,10 +177,11 @@ void Species::evolve() {
     generation_future  = generation_temporary;
 }
 
-std::ostream& operator<<(std::ostream &out, const Species &species) {
+template <int size>
+std::ostream& operator<<(std::ostream& out, const Species<size>& species) {
     for (const auto& row : *species.generation_current) {
         for (const auto& cell : row) {
-            if (cell == Species::Cell::ALIVE) {
+            if (cell == Species<size>::Cell::ALIVE) {
                 out << "# ";
             } else {
                 out << ' ';
@@ -182,20 +193,23 @@ std::ostream& operator<<(std::ostream &out, const Species &species) {
 }
 
 int main() {
-    std::array<std::array<Species::Cell, 10>, 10> generation_initial {};
-    generation_initial[2][5] = Species::Cell::ALIVE;
-    generation_initial[3][5] = Species::Cell::ALIVE;
-    generation_initial[4][5] = Species::Cell::ALIVE;
 
-    Species species_a(generation_initial);
+    const int size {10};
+
+    std::array<std::array<Species<size>::Cell, size>, size> generation_initial {};
+    generation_initial[2][5] = Species<size>::Cell::ALIVE;
+    generation_initial[3][5] = Species<size>::Cell::ALIVE;
+    generation_initial[4][5] = Species<size>::Cell::ALIVE;
+
+    Species<size> species_a(generation_initial);
     species_a.evolve();
     std::cout << species_a;
 
-    Species species_b(species_a);
+    Species<size> species_b(species_a);
     species_b.evolve();
     std::cout << species_b;
 
-    Species species_c(species_b);
+    Species<size> species_c(species_b);
     species_c = species_a;
     std::cout << species_c;
 
